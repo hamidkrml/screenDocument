0e5021772531ba140614cdb7af6274a9589bc8cc
hamidkrml <karimlihamid4@gmail.com>
2025-03-06 12:47:46 +0300

    feat: Document detail view added - Pages are now displayed in order - Page content is shown as an image - TabView is used for page navigation

---
+++ scannerDocumationAi/Views/Screens/LoginScreen/documentDetaliView.swift
@@ -0,0 +1,18 @@
+//
+//  documentDetaliView.swift
+//  scannerDocumationAi
+//
+//  Created by hamid on 06.03.25.
+//
+
+import SwiftUI
+
+struct documentDetaliView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    documentDetaliView()
+}
